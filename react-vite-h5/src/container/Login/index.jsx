a16b304c0e9e5cea68e8260419e263f0870ca1d2
wangxiaobai2019 <1510106069@qq.com>
2022-06-06 15:59:51 +0800

    chore: classnames feat: 登录

--- react-vite-h5/src/container/Login/index.jsx
+++ react-vite-h5/src/container/Login/index.jsx
@@ -2,6 +2,7 @@ import { useState, useCallback } from 'react'
 import { Cell, Button, Input, Checkbox, Toast } from 'zarm'
 import CustomIcon from '@/components/CustomIcon'
 import Captcha from 'react-captcha-code'
+import classnames from 'classnames'
 import { post } from '@/utils'
 import style from './style.module.less'
 
@@ -10,6 +11,7 @@ const Login = () => {
   const [password, setPassword] = useState('') // 密码
   const [verify, setVerify] = useState('') // 验证码
   const [captcha, setCaptcha] = useState('') // 验证码存值
+  const [type, setType] = useState('login') // 登录注册类型
 
   const handleChange = useCallback((captcha) => {
     console.log('captcha', captcha)
@@ -25,30 +27,47 @@ const Login = () => {
       Toast.show('请输入密码')
       return
     }
-    if (!verify) {
-      Toast.show('请输入验证码')
-      return
+    if (type === 'login') {
+      post('/user/login', {
+        username,
+        password
+      }).then(res => {
+        const {
+          data: {token}
+        } = res
+        localStorage.setItem('token', token)
+      }).catch(e => {
+        console.error(e)
+        Toast.show('系统错误')
+      })
+    } else {
+      if (!verify) {
+        Toast.show('请输入验证码')
+        return
+      }
+      if (verify !== captcha) {
+        Toast.show('验证码错误')
+        return
+      }
+      post('/user/register', {
+        username,
+        password
+      }).then(res => {
+        Toast.show('注册成功')
+        setType('login')
+      }).catch(e => {
+        console.error(e)
+        Toast.show('系统错误')
+      })
     }
-    if (verify !== captcha) {
-      Toast.show('验证码错误')
-      return
-    }
-    post('/user/register', {
-      username,
-      password
-    }).then(res => {
-      Toast.show('注册成功')
-    }).catch(e => {
-      console.error(e)
-      Toast.show('系统错误')
-    })
   }
 
   return (
     <div className={style.auth}>
       <div className={style.head} />
       <div className={style.tab}>
-        <span>注册</span>
+        <span className={classnames({[style.active]: type === 'login'})} onClick={() => setType('login')}>登录</span>
+        <span className={classnames({[style.active]: type === 'register'})} onClick={() => setType('register')}>注册</span>
       </div>
       <div className={style.form}>
         <Cell icon={<CustomIcon type="zhanghao" />}>
@@ -67,22 +86,30 @@ const Login = () => {
             onChange={value => setPassword(value)}
           />
         </Cell>
-        <Cell icon={<CustomIcon type="mima" />}>
-          <Input
-            clearable
-            type="text"
-            placeholder="请输入验证码"
-            onChange={value => setVerify(value)}
-          />
-          <Captcha charNum={4} onChange={handleChange} />
-        </Cell>
+        {
+          type === 'register' ? (
+            <Cell icon={<CustomIcon type="mima" />}>
+              <Input
+                clearable
+                type="text"
+                placeholder="请输入验证码"
+                onChange={value => setVerify(value)}
+              />
+              <Captcha charNum={4} onChange={handleChange} />
+            </Cell>
+          ) : null
+        }
       </div>
       <div className={style.operation}>
-        <div className={style.agree}>
-          <Checkbox />
-          <label className="text-align">阅读并同意<a>《手札条款》</a></label>
-        </div>
-        <Button onClick={onSubmit} block theme="primary">注册</Button>
+        {
+          type === 'register' ? (
+            <div className={style.agree}>
+              <Checkbox />
+              <label className="text-align">阅读并同意<a>《手札条款》</a></label>
+            </div>
+          ) : null
+        }
+        <Button onClick={onSubmit} block theme="primary">{type === 'register' ? '注册' : '登录'}</Button>
       </div>
     </div>
   )
